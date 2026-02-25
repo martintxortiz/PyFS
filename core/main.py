7e336bb83a5965f18c1d978a531dc0253b3cced3
lowg0d <lowgod_@hotmail.com>
2026-02-25 21:27:27 +0100

    test

---
+++ core/main.py
@@ -0,0 +1,11 @@
+class FLightSystemService:
+    def __init__(self):
+        pass
+
+    def run(self):
+        ...
+        # setup logging
+        # start core services (including mission)
+        # start core apps
+        # start user apps
+        #
---
+++ run.py
@@ -0,0 +1,5 @@
+from src.services.flight_system import FLightSystem
+
+if __name__ == "__main__":
+    fs = FLightSystem()
+    raise SystemExit(fs.run())
